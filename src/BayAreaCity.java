3aa21563beda8bff356496df091dff7b8b96cb18
Dennis Dang <dang.dennis21@gmail.com>
2017-06-19 00:04:33 -0700

    Contains our data

---
+++ src/BayAreaCity.java
@@ -0,0 +1,15 @@
+public class BayAreaCity {
+	private String name;
+	
+	public BayAreaCity(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String toString() {
+		return getName();
+	}
+}
