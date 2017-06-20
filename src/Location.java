bdfcbe2f964aa3a9fa9b93f38d736e95e8e7c6f3
wkdslqn <kjh9643@gmail.com>
2017-06-19 18:30:32 -0700

    Create Location.java

---
+++ src/Location.java
@@ -0,0 +1,41 @@
+public class Location {
+	private String name;
+
+	public Location(String name) {
+		setName(name);
+	}
+
+	public Location() {
+		this("");
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public String toString() {
+		return name;
+	}
+
+	@Override
+	public boolean equals(Object anotherLocation) {
+		boolean isLocation = anotherLocation instanceof Location;
+		if (!isLocation)
+			return false;
+		return name.equals(((Location) anotherLocation).getName());
+	}
+
+	@Override
+	public int hashCode() {
+		int hashCode = 0;
+		
+		for (int k = 0; k < name.length(); k++)
+			hashCode = 37 * hashCode + name.charAt(k);
+		return hashCode;
+	}
+}
