69be82b85b292ed9e93e48967a0b9c6dd6c774a1
Dennis Dang <dang.dennis21@gmail.com>
2017-06-22 14:23:15 -0700

    updated menu functionalities

--- src/Team6Driver.java
+++ src/Team6Driver.java
@@ -8,9 +8,9 @@ import java.util.Scanner;
 
 public class Team6Driver {
 	private static Scanner userScanner = new Scanner(System.in);
-	private static HashMap<String, Location> availableLocations;
-	private static EulerPath<Location> eulerPath;
-	private static Visitor<Location> locationVisitor = new LocationVisitor();
+	private static HashMap<String, String> availableLocations = new HashMap<>();
+	private static EulerPath<String> eulerPath;
+	private static Visitor<String> locationVisitor = new LocationVisitor();
 
 	public static void main(String[] args) throws IOException {
 		int userChoice = 0;
@@ -21,37 +21,7 @@ public class Team6Driver {
 		}
 	}
 
-	public static HashMap<String, Location> readInputFile() {
-		Scanner inputFile = openInputFile();
-		if (inputFile == null) {
-			System.out.println("\tUnable To Open Input File");
-			System.exit(0);
-		}
-		System.out.println("\tSuccessfully Opened Input File");
-		if (inputFile.hasNext() == false) {
-			System.out.println("\tInput File Is Empty");
-			System.exit(0);
-		}
-		HashMap<String, Location> availableLocations = new HashMap<>();
-		Location currentLocation;
-		while (inputFile.hasNext()) {
-			String[] vertices = inputFile.nextLine().split("[,]");
-			currentLocation = availableLocations.get(vertices[0]);
-			if (currentLocation == null) {
-				currentLocation = new Location(vertices[0]);
-				availableLocations.put(vertices[0], currentLocation);
-			}
-			for (int i = 1; i < vertices.length; i++) {
-				Location adjacentLocation = availableLocations.get(vertices[i]);
-				if (adjacentLocation == null) {
-					adjacentLocation = new Location(vertices[i]);
-					availableLocations.put(vertices[i], adjacentLocation);
-				}
-				eulerPath.addEdge(currentLocation, adjacentLocation, 0);
-			}
-		}
-		return availableLocations;
-	}
+
 
 	public static int showMenu() throws IOException {
 		int trialLimit = 3;
@@ -65,15 +35,16 @@ public class Team6Driver {
 			System.out.println("\t\t5. Display Graph");
 			System.out.println("\t\t6. Depth First Traversal");
 			System.out.println("\t\t7. Breadth First Traversal");
-			System.out.println("\t\t8. Find Euler Path");
-			System.out.println("\t\t9. Save as Text File");
-			System.out.println("\t\t10. Exit");
+			System.out.println("\t\t8. Show adjacency list");
+			System.out.println("\t\t9. Find Euler Path");
+			System.out.println("\t\t10. Save as Text File");
+			System.out.println("\t\t11. Exit");
 			System.out.println();
 			System.out.print("\tYour Choice: ");
 			userScanner = new Scanner(System.in);
 			try {
 				int userChoice = userScanner.nextInt();
-				if (userChoice < 1 || userChoice > 10)
+				if (userChoice < 1 || userChoice > 11)
 					System.out.println("\n\tInvalid Number! Please Type Another Number.");
 				else
 					return userChoice;
@@ -95,12 +66,14 @@ public class Team6Driver {
 		case 1:
 			eulerPath = new EulerPath<>();
 			userScanner.nextLine();
-			availableLocations = readInputFile();
-			eulerPath.checkForEulerPath();
+			availableLocations = readInputFile(eulerPath);
+			System.out.println("File read");
+			System.out.println("Press <Enter> to continue");
 			break;
 		case 2:
-			if (eulerPath != null)
+			if (eulerPath != null) {
 				addingEdge();
+			}
 			else
 				System.out.println("\tYou have to read input file first");
 			break;
@@ -135,19 +108,31 @@ public class Team6Driver {
 				System.out.println("\tYou have to read input file first");
 			break;
 		case 8:
-			if (eulerPath != null)
-				findingEulerPath();
+			if (eulerPath != null) {
+				eulerPath.showAdjTable();
+			}
+
 			else
 				System.out.println("\tYou have to read input file first");
 			break;
 		case 9:
-			if (eulerPath != null)
-				saveEulerPathAsTextFile();
+			if (eulerPath != null) {
+				eulerPath.checkForEulerPath();
+			}
+
 			else
 				System.out.println("\tYou have to read input file first");
 			break;
 		case 10:
+			if (eulerPath != null) {
+				saveEulerPathAsTextFile();
+			}
+			else
+				System.out.println("\tYou have to read input file first");
+			break;
+		case 11:
 			System.out.println("\tThank You For Using The Program");
+			System.exit(0);
 			break;
 		}
 	}
@@ -164,12 +149,12 @@ public class Team6Driver {
 		}
 		System.out.print("\n\tEnter Location 2: ");
 		String location2 = userScanner.nextLine();
-		if (!availableLocations.containsKey(location2)) {
-			System.out.println("\n\tFailed To Add Edge.\n\t\"" + location2 + "\" Is Not In The Graph.");
-			return;
-		}
-		Location source = availableLocations.get(location1);
-		Location destination = availableLocations.get(location2);
+		//if (!availableLocations.containsKey(location2)) {
+		//	System.out.println("\n\tFailed To Add Edge.\n\t\"" + location2 + "\" Is Not In The Graph.");
+		//	return;
+		//}
+		String source = location1;
+		String destination = location2;
 		eulerPath.addEdge(source, destination, 0);
 		System.out.println("\n\tSuccessfully Added Edges Between \"" + location1 + "\" And \"" + location2 + "\"");
 	}
@@ -190,8 +175,8 @@ public class Team6Driver {
 			System.out.println("\tFailed To Add Edge.\n\t\"" + location2 + "\" Is Not In The Graph.");
 			return;
 		}
-		Location source = availableLocations.get(location1);
-		Location destination = availableLocations.get(location2);
+		String source = availableLocations.get(location1);
+		String destination = availableLocations.get(location2);
 		if (eulerPath.remove(source, destination))
 			System.out.println("\tSuccessfully Removed Edges Between \"" + location1 + "\" And \"" + location2 + "\"");
 	}
@@ -232,7 +217,7 @@ public class Team6Driver {
 		System.out.println("\t\tDemonstrate Traversal\n");
 		userScanner.nextLine();
 		System.out.print("\tEnter Starting Location: ");
-		Location startLocation = availableLocations.get(userScanner.nextLine());
+		String startLocation = availableLocations.get(userScanner.nextLine());
 		if (startLocation != null) {
 			System.out.print("\n\t___ DEPTH FIRST TRAVERSAL ___\n");
 			eulerPath.depthFirstTraversal(startLocation, locationVisitor);
@@ -247,7 +232,7 @@ public class Team6Driver {
 		System.out.println("\t\tDemonstrate Traversal\n");
 		userScanner.nextLine();
 		System.out.print("\tEnter Starting Location: ");
-		Location startLocation = availableLocations.get(userScanner.nextLine());
+		String startLocation = availableLocations.get(userScanner.nextLine());
 		if (startLocation != null) {
 			System.out.print("\n\n\t___ BREADTH FIRST TRAVERSAL ___\n");
 			eulerPath.breadthFirstTraversal(startLocation, locationVisitor);
@@ -256,63 +241,9 @@ public class Team6Driver {
 			System.out.println("\t\"" + startLocation + "\" Is Not In The Graph");
 		}
 	}
-
-	public static void findingEulerPath() {
-		System.out.println("\t-----------------------------------------------------");
-		System.out.println("\t\tFind Euler Path\n");
-		userScanner.nextLine();
-		System.out.print("\tEnter Starting Location: ");
-		String locationName = userScanner.nextLine();
-		Location startLocation = availableLocations.get(locationName);
-		if (startLocation != null) {
-			if (eulerPath.isEulerPath()) {
-				eulerPath.findEulerPath(eulerPath.vertexSet.get(startLocation));
-				System.out.println("\n\n\tDo You Want To Save Solution? (Yes/No)");
-				System.out.print("\tYour Choice: ");
-				// userScanner.nextLine();
-				boolean yes = userScanner.nextLine().equalsIgnoreCase("yes");
-				if (yes) {
-					String filename;
-					System.out.print("\n\tEnter Where You Want To Save: ");
-					filename = userScanner.nextLine();
-					File file = new File(filename);
-					try {
-						eulerPath.saveEulerPath(new PrintWriter(file));
-						System.out.println("\tSuccessfully Saved Euler Path In " + filename);
-					} catch (IOException exception) {
-						System.out.println("\tFailed To Save Euler ");
-					}
-				}
-			}
-		} else {
-			System.out.println("\t\"" + locationName + "\" Is Not In The Graph");
-		}
-	}
-
-	public static void saveEulerPathAsTextFile() {
-		System.out.println("\t-----------------------------------------------------");
-		System.out.println("\t\tSave \n");
-		userScanner.nextLine();
-		String filename;
-		System.out.print("\tEnter Where You Want To Save: ");
-		filename = userScanner.nextLine();
-		File file = new File(filename);
-		try {
-			eulerPath.saveAsTextFile(new PrintWriter(file));
-			System.out.println("Successfully Save The Graph As Text File In" + filename);
-		} catch (IOException exception) {
-			System.out.println("\tFailed To Save The Graph As Text File");
-		}
-	}
-
-	/**
-	 * 
-	 * Open an input file to read
-	 * 
-	 * @return a Scanner to read input file
-	 * 
-	 */
+	
 	public static Scanner openInputFile() {
+		System.out.println("in");
 		String filename;
 		Scanner scanner = null;
 		System.out.print("\tEnter the input filename: ");
@@ -327,30 +258,46 @@ public class Team6Driver {
 		} // end catch
 		return scanner;
 	}
-	
-	public static void readInputFile(Graph<String> graph) {
-			Scanner inputFile = openInputFile();
-			if (inputFile == null) {
-				System.out.println("No input, code bricked. Try again");
-				System.exit(1);
-			}
-			String temp = null;
-			String city = null, neighbor = null;
+	public static void saveEulerPathAsTextFile() {
+		System.out.println("\t-----------------------------------------------------");
+		System.out.println("\t\tSave \n");
+		userScanner.nextLine();
+		String filename;
+		System.out.print("\tEnter Where You Want To Save: ");
+		filename = userScanner.nextLine();
+		File file = new File(filename);
+		try {
+			eulerPath.saveAsTextFile(new PrintWriter(file));
+			System.out.println("Successfully Save The Graph As Text File In: " + filename);
+		} catch (IOException exception) {
+			System.out.println("\tFailed To Save The Graph As Text File");
+		}
+	}
+
+	public static HashMap<String, String> readInputFile(Graph<String> graph) {
+		Scanner inputFile = openInputFile();
+		if (inputFile == null) {
+			System.out.println("No input, code bricked. Try again");
+			System.exit(1);
+		}
+		String temp = null, city = null, neighbor = null;
+		temp = inputFile.nextLine();
+		city = temp;
+		boolean isName = true;
+		while (inputFile.hasNextLine()) {
 			temp = inputFile.nextLine();
-			city = temp;
-			boolean isName = true;
-			while (inputFile.hasNextLine()) {
-				temp = inputFile.nextLine();
-				if (temp.equals("x")) {
-					isName = !isName;
+			if (temp.equals("x")) {
+				isName = !isName;
+			} else {
+				if (isName) {
+					city = temp;
 				} else {
-					if (isName) {
-						city = temp;
-					} else {
-						neighbor = temp;
-						graph.addEdge(city, neighbor, 0);
-					}
+					neighbor = temp;
+					graph.addEdge(city, neighbor, 0);
+					availableLocations.put(city, neighbor);
 				}
 			}
 		}
+		return availableLocations;
+	}
 }
